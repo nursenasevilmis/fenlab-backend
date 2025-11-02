54efc94b073aa429bb20dd352fed7c5abac7ac15
nursena <nursenasevilmis91@gmail.com>
2025-11-03 01:07:23 +0300

    Ortam ve ders enumları oluşturuldu.

---
+++ src/main/kotlin/com/nursenasevilmis/fenlab/model/enums/EnvironmentType.kt
@@ -0,0 +1,7 @@
+package com.nursenasevilmis.fenlab.model.enums
+
+enum class EnvironmentType {
+    HOME,
+    LABORATORY,
+    OUTDOOR
+}
---
+++ src/main/kotlin/com/nursenasevilmis/fenlab/model/enums/SubjectType.kt
@@ -0,0 +1,9 @@
+package com.nursenasevilmis.fenlab.model.enums
+
+enum class SubjectType {
+    SCIENCE,
+    PHYSICS,
+    CHEMISTRY,
+    BIOLOGY,
+    OTHER
+}
