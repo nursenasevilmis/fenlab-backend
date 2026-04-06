4004510f5993575f39e2cffb4a14e9d2adc584ab
nursenasevilmis <nursenasevilmis91@gmail.com>
2026-04-06 03:22:28 +0300

    redis kaldırıldı

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,7 @@ repositories {
 
 dependencies {
 	implementation("io.github.microutils:kotlin-logging-jvm:3.0.5")
-	implementation("org.springframework.boot:spring-boot-starter-data-redis")
+	//implementation("org.springframework.boot:spring-boot-starter-data-redis")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
