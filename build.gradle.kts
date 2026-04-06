2ff8362d6dbe857a6cb520d35bad9c4de8bf88e6
nursenasevilmis <nursenasevilmis91@gmail.com>
2026-04-06 04:05:25 +0300

    cloudinary eklendi

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,6 +21,7 @@ repositories {
 }
 
 dependencies {
+	implementation("com.cloudinary:cloudinary-http44:1.36.0")
 	implementation("io.github.microutils:kotlin-logging-jvm:3.0.5")
 	//implementation("org.springframework.boot:spring-boot-starter-data-redis")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
